0be9fba3f7d299b21b75b34c5c6189b3c855ec3d
Andrew Romanov <scorpion21.97@gmail.com>
2016-08-19 19:15:47 +0300

    Add Routes.jsx

--- source/scripts/index.jsx
+++ source/scripts/index.jsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import Routes from './Routes';
 
 
 document.addEventListener('deviceready', onDeviceReady);
 
 function onDeviceReady() {
-	ReactDOM.render(<App />, document.querySelector('#app'));
+	ReactDOM.render(<Routes />, document.querySelector('#app'));
 }
