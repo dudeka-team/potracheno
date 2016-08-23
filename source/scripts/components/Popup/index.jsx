a0b2bfb4904f8a6b3bf17520f548f065e70d65b1
Daniil <rulir93@gmail.com>
2016-08-23 21:44:15 +0400

    popup module. popup header. popup footer

--- source/scripts/components/Popup/index.jsx
+++ source/scripts/components/Popup/index.jsx
@@ -1,13 +1,18 @@
 import React from 'react';
-import {TopBar, TopBarHeading, TopBarIcon} from '../TopBar';
 
-const Popup = React.createClass({
+export const Popup = React.createClass({
     render() {
         return (
         	<div className="popup-container">
 	            <div className="popup-layout"></div>
 	        	<div className="popup-wrapper">
 	        		<div className="popup-inner">
+				        <div className="popup-header">
+				        	{this.props.closeIcon && <div className="popup-header__icon popup-header__icon_burger"></div>}
+				            <div className="popup-header__title">
+				            	{this.props.title}
+				            </div>
+			        	</div>	
 	        			{this.props.children}
 	        		</div>
 	        	</div>
@@ -16,4 +21,12 @@ const Popup = React.createClass({
     }
 });
 
-module.exports = Popup;
+export const PopupFooter = React.createClass({
+    render() {
+        return (
+        	<div className="popup-footer">
+	            {this.props.children}
+        	</div>
+        );
+    }
+});
