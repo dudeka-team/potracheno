f55b988809b6b4eeff974abca79e2b820a1a0093
Aspirationtocode <aspiretocode@gmail.com>
2016-08-24 01:51:55 +0300

    remove markup to resolve conflicts

--- source/scripts/pages/EventsPage.jsx
+++ source/scripts/pages/EventsPage.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
-import ModalContentPayers from '../components/ModalContentPayers'
 
 export default function EventsPage() {
 	return (
 		<div>
-			<ModalContentPayers />
 			<div className="events-page">Events page</div>
 		</div>
 	);
