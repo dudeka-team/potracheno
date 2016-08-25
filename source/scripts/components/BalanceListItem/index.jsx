aaf206866ee896fb70769fe411100d95c70de5a7
Daniil <rulir93@gmail.com>
2016-08-25 23:52:10 +0400

    sum calculation, flex

---
+++ source/scripts/components/BalanceListItem/index.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+class BalanceListItem extends React.Component {
+	render() {
+		let owner = this.props.curUser.name;
+		let listItems = this.props.data.map(item => {
+			return (
+				(owner.indexOf(item.owner) !== -1) &&
+
+				item.members.map(member => {
+					if (member !== owner) {
+						return (
+							<li className="balance-list-item" key={item.member}>
+								<div className="balance-list-item__direction">
+									Вам 
+									<span className="balance-list-item__icon balance-list-item__icon_to-you" />
+									{`${member}`}
+								</div>
+								<div className="balance-list-item__sum">{Math.round(item.sum/item.members.length)}</div>
+							</li>
+						)
+					}
+				})
+			);
+		});
+		return (
+			<div>
+				{listItems}
+			</div>
+		);
+	}
+}
+
+export default BalanceListItem;
--- source/scripts/pages/BalancePage.jsx
+++ source/scripts/pages/BalancePage.jsx
@@ -1,10 +1,34 @@
 import React from 'react';
 import BlueSubtitle from '../components/BlueSubtitle';
+import List from '../components/List';
+import BalanceListItem from '../components/BalanceListItem';
+
+let purchases = [
+	{	
+		owner: 'Дамир',
+		members: ['Дамир', 'Дан', 'Костя', 'Андрей', 'Юра', 'Женя'],
+		sum: 4000,
+		id: 1,
+	},
+	{	
+		owner: 'Костя',
+		members: ['Дамир', 'Дан', 'Костя', 'Андрей', 'Юра', 'Женя'],
+		sum: 3000,
+		id: 2,
+	},
+];
+
+let curUser = {
+	name: 'Дамир',
+};
 
 export default function BalancePage() {
 	return (
 		<div className="balance-page">
 			<BlueSubtitle text="Ваш баланс"/>
+			<List>
+				<BalanceListItem data={purchases} curUser={curUser} />
+			</List>
 		</div>
 	);
 }
