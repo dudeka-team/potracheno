dd83a1b41549eaae345842e393301b4181587dbc
Andrew Romanov <scorpion21.97@gmail.com>
2016-08-25 17:28:30 +0300

    Add 'new event' pages

--- source/scripts/Routes.jsx
+++ source/scripts/Routes.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Router, Route, IndexRedirect} from 'react-router';
 import App from './App';
 import EventsPage from './pages/EventsPage';
+import NewEventPage from './pages/NewEvent';
 import BalancePage from './pages/BalancePage';
 import EventPage from './pages/EventPage';
 import PurchasePage from './pages/PurchasePage';
@@ -12,6 +13,7 @@ export default function Routes(props) {
 			<Route path="/" component={App}>
 				<IndexRedirect to="events" />
 				<Route path="events" component={EventsPage} />
+				<Route path="events/new" component={NewEventPage} />
 				<Route path="balance" component={BalancePage} />
 				<Route path="event" component={EventPage} />
 				<Route path="purchase" component={PurchasePage} />
