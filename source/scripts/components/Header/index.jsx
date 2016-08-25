dce55cdb61551f996343c53a6340be988176614d
Aspirationtocode <aspiretocode@gmail.com>
2016-08-25 15:24:21 +0300

    add required prop to header

--- source/scripts/components/Header/index.jsx
+++ source/scripts/components/Header/index.jsx
@@ -2,8 +2,8 @@ import React from 'react';
 
 const Header = React.createClass({
 	render() {
-		return <div className='header'>{this.props.text}</div>
-	}
-})
+		return <div className="header">{this.props.text}</div>;
+	},
+});
 
 export default Header;
