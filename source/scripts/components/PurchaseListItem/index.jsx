0f062702a59782b2d44a2b0cc0349c2fd493866e
Aspirationtocode <aspiretocode@gmail.com>
2016-08-27 14:52:37 +0300

    add purchaselititem

--- source/scripts/components/PurchaseListItem/index.jsx
+++ source/scripts/components/PurchaseListItem/index.jsx
@@ -4,11 +4,11 @@ export default function PurchaseListItem(props) {
 	return (
 		<li className="purchase-list-item">
 			<div className="purchase-list-item__descr">
-				<div className="purchase-list-item__buyer">{props.buyer}</div>
-				<div className="purchase-list-item__title">{props.title}</div>
-				<div className="purchase-list-item__subtitle">{props.subtitle}</div>
+				<div className="purchase-list-item__buyer">{ props.buyer }</div>
+				<div className="purchase-list-item__title">{ props.title }</div>
+				<div className="purchase-list-item__subtitle">{ props.subtitle }</div>
 			</div>
-			<div className="purchase-list-item__price">{props.price} р</div>
+			<div className="purchase-list-item__price">{ props.price } р</div>
 		</li>
 	);
 }
