b359ffda6df97f3bf13f386a805f04a5aefd32d7
Andrew Romanov <me@andrew-r.ru>
2017-06-13 21:16:51 +0300

    Remove OneSignal API call

--- source/scripts/actions/getLocalEvents.js
+++ source/scripts/actions/getLocalEvents.js
@@ -3,20 +3,8 @@ import {
 } from '../constants';
 import db from '../database';
 
-function getOnesignalTags(localEvents) {
-	const localEventsIds = Object.keys(localEvents);
-	const result = {};
-
-	localEventsIds.forEach((id) => {
-		result[id] = 1;
-	});
-
-	return result;
-}
-
 export default function getLocalEvents() {
 	const localEvents = db.getLocalEvents();
-	window.OneSignal.push(['sendTags', getOnesignalTags(localEvents)]);
 
 	return {
 		type: GET_LOCAL_EVENTS,
