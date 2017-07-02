3ef8d1fa519eb6b18d72cb06a68c83cfda39989e
Andrew Romanov <me@andrew-r.ru>
2017-07-02 23:10:01 +0300

    [#1] Add FormInput component

---
+++ source/scripts/components/form-input/form-input.story.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import FormInput from './form-input';
+
+storiesOf('FormInput', module)
+	.add('empty', () => (
+		<FormInput
+			type="number"
+			placeholder="Сумма, ₽"
+		/>
+	))
+	.add('with value', () => (
+		<FormInput
+			type="number"
+			value={5000}
+		/>
+	))
+	.add('disabled', () => (
+		<FormInput
+			disabled
+			value="Hello, world!"
+		/>
+	))
+	.add('large', () => (
+		<FormInput
+			size={FormInput.sizes.large}
+			value="Hello, world!"
+		/>
+	));
+
