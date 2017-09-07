989ac6e12bdf1b93201c28127c4cb82e4c875f61
Andrew Romanov <me@andrew-r.ru>
2017-09-08 00:22:29 +0300

    [#17] Add indeterminate state for Checkbox

--- source/scripts/components/checkbox/checkbox.story.js
+++ source/scripts/components/checkbox/checkbox.story.js
@@ -6,11 +6,17 @@ storiesOf('Checkbox', module)
 	.add('default', () => (
 		<Checkbox />
 	))
+	.add('indeterminate', () => (
+		<Checkbox indeterminate />
+	))
 	.add('checked', () => (
 		<Checkbox checked />
 	))
 	.add('disabled', () => (
 		<Checkbox disabled />
+	))
+	.add('disabled indeterminate', () => (
+		<Checkbox disabled indeterminate />
 	))
 	.add('disabled checked', () => (
 		<Checkbox disabled checked />
