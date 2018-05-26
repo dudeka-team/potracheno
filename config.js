5ad020dec691ef480d25b20b8ca88e655b41d8eb
alik0211 <alik0211alik@gmail.com>
2018-05-26 08:39:22 +0400

    [#20] Optimize your webpack config
    Variables: defaultEnv and nodeEnv are not used anywhere

--- config.js
+++ config.js
@@ -1,10 +1,6 @@
 const path = require('path');
 
-const defaultEnv = 'production';
-
 module.exports = {
-	defaultEnv,
-	nodeEnv: process.env.NODE_ENV || defaultEnv,
 	isAnalyzeModeEnabled: Boolean(process.env.ANALYZE),
 	devServerPort: process.env.PORT || 8080,
 	sourceDir: path.resolve(__dirname, './source'),
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,3 @@
-const { defaultEnv, nodeEnv } = require('./config');
-
-let config;
-
-try {
-	// eslint-disable-next-line global-require
-	config = require(`./webpack/${nodeEnv}.config`);
-} catch (error) {
-	console.error(error);
-	console.error(`Couldn't find webpack config for ${nodeEnv} environment, using ${defaultEnv} config as a fallback\n`);
-	// eslint-disable-next-line global-require
-	config = require(`./webpack/${defaultEnv}.config`);
-}
+const config = require(`./webpack/${process.env.NODE_ENV}.config`);
 
 module.exports = config;
