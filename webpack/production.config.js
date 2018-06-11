6a1008d67678614adb54731c1c2eceb909bafaba
Ali Gasymov <alik0211alik@gmail.com>
2018-06-11 18:51:32 +0400

    [#20] Migrate to webpack 4
    * Update webpack
    
    - Update dependencies for webpack
    - Use the mode option
    - Combine configs with webpack-merge
    
    * [#20] Optimize your webpack config
    
    Variables: defaultEnv and nodeEnv are not used anywhere
    
    * [#20] Use exact versions of webpack packages
    
    * Use mini-css-extract-plugin
    
    * [#20] Set the correct font paths

--- webpack/production.config.js
+++ webpack/production.config.js
@@ -1,45 +1,40 @@
-const ExtractTextPlugin = require('extract-text-webpack-plugin');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const baseWebpackConfig = require('./base.config');
+const merge = require('webpack-merge');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const common = require('./common.config');
 
-module.exports = {
-	...baseWebpackConfig,
+module.exports = merge(common, {
+	mode: 'production',
 	module: {
-		...baseWebpackConfig.module,
 		rules: [
-			...baseWebpackConfig.module.rules,
 			{
 				test: /\.styl$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: [
-						'postcss-loader',
-						'stylus-loader',
-					],
-				}),
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'postcss-loader',
+					'stylus-loader',
+				],
 			},
 			{
 				test: /\.css$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: [
-						{
-							loader: 'css-loader',
-							options: {
-								modules: true,
-								importLoaders: 1,
-								localIdentName: '[name]-[local]-[hash:base64:5]',
-							},
+				use: [
+					MiniCssExtractPlugin.loader,
+					{
+						loader: 'css-loader',
+						options: {
+							modules: true,
+							importLoaders: 1,
+							localIdentName: '[name]-[local]-[hash:base64:5]',
 						},
-						'postcss-loader',
-					],
-				}),
+					},
+					'postcss-loader',
+				],
 			},
 		],
 	},
 	plugins: [
-		...baseWebpackConfig.plugins,
-		new ExtractTextPlugin('app.css'),
-		new UglifyJsPlugin(),
+		new MiniCssExtractPlugin({
+			filename: 'app.css',
+		}),
 	],
-};
+});
